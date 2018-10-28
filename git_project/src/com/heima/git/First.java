dd52ad72d2c8a3247d9ea7158d659d0245120687
cf123 <cf577950356@163.com>
2018-10-28 20:55:30 +0800

    add success first

---
+++ git_project/src/com/heima/git/First.java
@@ -0,0 +1,11 @@
+package com.heima.git;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Git project");
+        System.out.println("Hello Git project");
+        System.out.println("Hello Git project");
+        System.out.println("Hello Git project");
+        System.out.println("Hello Git project");
+    }
+}
